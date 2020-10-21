54ad122948fc75b7538e1a63ad8150e1d23493ff
samkitnahar <46357024+samkitnahar@users.noreply.github.com>
2020-10-22 00:55:04 +0530

    Heroku

--- craigs_list/settings.py
+++ craigs_list/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ku3s&($&i_hm_qcs)jansnn9jccokxaf!(n73yu_q1y_gi6*ob'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shielded-sea-40908.herokuapp.com']
 
 
 # Application definition
