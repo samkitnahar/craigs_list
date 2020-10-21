84e4d1fa7536538788620eb14dd71963d7bdfb72
samkitnahar <46357024+samkitnahar@users.noreply.github.com>
2020-10-21 23:30:44 +0530

    Search & Web Scrapping

--- my_app/models.py
+++ my_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Search(models.Model):
     search = models.CharField(max_length=200) 
-    created = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField()
 
     def __str__(self):
         return '{}'.format(self.search)
--- my_app/views.py
+++ my_app/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
+from requests.compat import quote_plus
+from . import models
+from django.utils import timezone
+
+
+BASE_CRAIGSLIST_URL = 'https://indore.craigslist.org/search/sss?query={}'
+BASE_IMG_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search, created=timezone.now())
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class':'result-row'})
+    final_posting = []
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        post_price = post.find(class_='result-price').text
+        if post.find(class_='result-image').get('data-ids'):
+            a = post.find(class_='result-image').get('data-ids').split(':', 1)
+            post_image = BASE_IMG_URL.format(quote_plus(a[1]))
+        else:
+            post_image = 'https://craigslist.org/images/peace.jpg'
+        final_posting.append((post_title, post_url, post_price, post_image))
+
+
     frontend = {
-        'search': search
+        'search': search,
+        'final_posting': final_posting,
     }
     return render(request, 'my_app/new_search.html', frontend)
